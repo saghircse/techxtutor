6aacf03097dac5e5832f4e339e41f60b61436111
saghircse <saghircse@gmai.com>
2023-01-05 09:26:24 +0530

    Core Java

--- java/JavaTutorial/src/com/techxtutor/bean/Student.java
+++ java/JavaTutorial/src/com/techxtutor/bean/Student.java
@@ -7,28 +7,48 @@ public class Student {
 	int marks;
 	
 	// static variable - class variable
-	static int PASS_MARKS = 40;
+	static String COLLEGE = "DKC";
+	final static int PASS_MARKS = 40;
+	
+	
+	// Default Constructor
+	Student(){
+		System.out.println("I am default constructor.");
+	}
+	
+	// Parameterized Constructor
+	Student(int roll, String name, int marks){
+		System.out.println("I am parameterized constructor.");
+		this.roll = roll;
+		this.name = name;
+		this.marks = marks;
+	}
+	
+	// Parameterized Constructor
+	Student(int roll, String name){
+		System.out.println("I am parameterized constructor.");
+		this.roll = roll;
+		this.name = name;
+	}
+	
 	
 	// Behaviors are represented using instance methods
 	boolean isPass(){
-		
 		if(marks >= PASS_MARKS){
 			return true;
 		}else{
 			return false;
-		}
-		
+		}	
 	}
 	
 	// Static method to display object
-	static void displayX(Student s) {
-		System.out.println(s.roll + " | " + s.name + " | " + s.marks + " | " + s.isPass() );
+	static void display(Student s) {
+		System.out.println(COLLEGE +" | "+ s.roll + " | " + s.name + " | " + s.marks + " | " + s.isPass() );
 	}
 	
 	// Instance method to display object
 	void display() {
-		System.out.println(roll + " | " + name + " | " + marks + " | " + isPass() );
-		System.out.println("------------------------");
+		System.out.println(COLLEGE+ " | " +roll + " | " + name + " | " + marks + " | " + isPass() );
 	}
 	
 	// static method (class method) to set student object
@@ -54,12 +74,14 @@ public class Student {
 		
 		// 1. Initialize object using reference variable
 		Student s1 = new Student();
+		s1.COLLEGE = "VKU";
 		System.out.println(s1.roll + " | " + s1.name + " | " + s1.marks + " | " + s1.isPass() );
 		s1.roll = 1;
 		s1.name = "John";
 		s1.marks = 60;
-		s1.display();
+		display(s1);
 		
+		System.out.println("================================================");
 		
 		// 2. Initialize object using static method
 		Student s2 = new Student();
@@ -67,13 +89,28 @@ public class Student {
 		setStudentX(s2, 2, "San", 65);
 		s2.display();
 		
+		System.out.println("================================================");
+		
 		// 3. Initialize object using instance method
 		Student s3 = new Student();
 		s3.display();
 		s3.setStudent(3, "Fan", 55);
 		s3.display();
 		
+		System.out.println("================================================");
+		// How to initialize any object at the time of its creation?
 		// 4. Initialize object using constructors
+		Student s4 = new Student(4, "Tan", 70);
+		s4.display();
+		
+		System.out.println("================================================");
+		// How to initialize any object at the time of its creation?
+		// 4. Initialize object using constructors
+		Student s5 = new Student(5, "Tan");
+		//Student.PASS_MARKS =30; // Final variable cannot be modified
+		s5.marks=35;
+		s5.name = "Jan";
+		s5.display();
 		
 		
 
