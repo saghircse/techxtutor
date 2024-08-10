60f4a7d21a993b106b2e27cb07948c958e9f217f
saghircse <saghircse@gmai.com>
2024-08-10 10:58:00 +0530

    C day-6 loops

--- c/c-day-06.c
+++ c/c-day-06.c
@@ -13,6 +13,9 @@ x = 5; // variable initialization/assignment
 
 int x = 5; //variable declaration + initialization
 ------------------------------
+
+loop 
+iteration
 1. for loop syntax:
 for(Initialization; Condition; Change ){
 	// statement-1
@@ -20,7 +23,45 @@ for(Initialization; Condition; Change ){
 	::::
 	// statement-n
 }
+----------------------------------
+2. while loop syntax:
+Initialization
+while(Condition){
+	// statement-1
+	// statement-2
+	::::
+	// statement-n
+	Change
+}
+-> Entry controlled loop
+-----------------------------------
+Initialization
+do{
+	// statement-1
+	// statement-2
+	::::
+	// statement-n
+	Change
+}
+while(Condition);
+-> Exit controlled loop
+-> loop body is executed at least once.
 
+int i=6;
+while(i<=5)
+{
+	printf("%d ", i);
+	i++;
+}
+
+int i=6;
+do
+{
+	printf("%d ", i);
+	i++;
+}
+while(i<=5);
+-----------------------------------
 Q1. Write a program to print numbers from 1 to 5.
 for(int i=1; i<= 5; i++)
 {
@@ -42,6 +83,14 @@ for(; i<= 5; i++)
 i => 1 -> 2 -> 3 -> 4 -> 5 -> 6
 --------
 1 2 3 4 5 
+
+--------------------------while loop---
+int i=1; // initialization
+while(i<=5) // condition
+{
+	printf("%d ", i);
+	i++; // Change
+}
 =================================
 Q2. Write a program to print numbers from 1 to n where n is given by user.
 #include <stdio.h>
@@ -90,6 +139,86 @@ int main() {
 }
 ---------------------------------
 Q5. Write a program to find the sum of all numbers between two given numbers.
+Q6. Write a program to find the sum of even numbers and odd numbers between two given numbers.
+Q7. Factorial of a number.
+Q8. Find the reverse of a number.
+	-> Also Check if a number is palindrome or not. 
+		-> 121, 3223 : palindrome numbers
+		-> 123 not palindrome
+		
+n=235
+rn=0
+---------iteration=1
+r = n%10 = 5
+rn = rn*10 + r = rn*10 + 5 = 5
+n = n/10 = 23
+---------iteration=2
+r = n%10 = 3
+rn = rn*10 + r = rn*10 + 3 = 53
+n = n/10 = 2
+---------iteration=3
+r = n%10 = 2
+rn = rn*10 + r = rn*10 + 2 = 532
+n = n/10 = 0
+------
+
+#include <stdio.h>
+int main() {
+    int n,rn, r, t;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    rn = 0;
+    t = n;
+    while(t!=0){
+	    r=t%10;
+	    rn=rn*10 + r;
+    	t=t/10;
+    }
+    printf("Reverse of %d = %d\n", n, rn);
+    if(n==rn){
+	    printf("%d is a palindrome", n);
+    }
+    else{
+	    printf("%d is not a palindrome", n);
+    }
+    return 0;
+}
+
+Q9. Check if a number is prime or not.
+Q10. Print the following patterns.
+---------a------
+n=5
+*
+**
+***
+****
+*****
+
+1
+22
+333
+4444
+55555
+
+1
+23
+456
+---------b------
+n=4
+   *
+  **
+ ***
+****
+---------c------
+   *
+  ***
+ *****
+*******
+
+
+
+
+
 
 
 
