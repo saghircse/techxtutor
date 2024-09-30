5f30ef03dcda8fb542d07e5f566dd7a4dcdc9388
saghircse <saghircse@gmai.com>
2024-09-30 23:01:43 +0530

    C function

--- c/c-part-08.c
+++ c/c-part-08.c
@@ -11,6 +11,7 @@ Advantage:
 --------------------------------------------------------------
 Variable:
 dataType varName = <value>;
+int x=5;
 
 => Function Declaration:
 returnType functionName(dataType arg1, dataType arg2,... );
@@ -54,5 +55,77 @@ int cubeSum(int a, int b) {
 	return cs;
 }
 
+-----------------------
+Variety of function:
+1. No any input and returns nothing 
+    => void sayHello().
+2. Takes input, but doesn't return anything.
+    => void printCubeSum(int a, int b)
+3. Takes input and returns value. 
+    => int cubeSum(int,int)
+4. No any input, but returns value 
+    => int main()
+
+Program 1:
+#include <stdio.h>
+void printReverseOfNum(int x);
+int  reverseOfNum(int x);
+int largestNum(int a, int b, int c);
+int main(){
+	int x1=1234, x2=5346, x3=9840;
+	printReverseOfNum(x1);
+	printReverseOfNum(x2);
+	printReverseOfNum(x3);
+	
+	// Program to find largest of reverse of x1, x2 and x3
+	int rx1 = reverseOfNum(x1);
+	int rx2 = reverseOfNum(x2);
+	int rx3 = reverseOfNum(x3);
+	
+	int big = largestNum(rx1, rx2, rx3);
+	printf("\nLargest of reverse of x1, x2 and x3 : %d", big);
+	
+	return 0;
+}
 
 
+int largestNum(int a, int b, int c)
+{
+    int big;
+    if(a >= b && a >= c){
+        big = a;
+    }else if (b >= a && b >= c){
+        big = b;
+    }else{
+        big = c;
+    }
+    return big;
+}
+
+void printReverseOfNum(int x)
+{
+	int rn=0,r,n=x;
+	while(n>0)
+	{
+		r = n%10;
+		rn = rn*10 + r;
+		n = n/10;
+	}
+	printf("\nReverse of %d is %d",x,rn );
+}
+
+int reverseOfNum(int x)
+{
+	int rn=0,r,n=x;
+	while(n>0)
+	{
+		r = n%10;
+		rn = rn*10 + r;
+		n = n/10;
+	}
+	return rn;
+}
+
+Question-1:
+Write a program to find the sum of all the prime in a range.
+	-> Use function when required.
