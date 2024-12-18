5cb1e8864d63bf2abbdda427f1d402d33919f49e
saghircse <saghircse@gmai.com>
2024-12-18 22:14:12 +0530

    dsa stack using c

--- c/dsa/dsa-part-1.c
+++ c/dsa/dsa-part-1.c
@@ -33,55 +33,79 @@ Basic operation in Stack:
 4. isEmpty()
 5. isFull()
 
-#include <stdio.h>
-
-#define MAXSIZE 100
+#define MAXSIZE 5
 int stack[MAXSIZE];
 int TOP = -1;
 
+void push(int x);
+int pop();
+int peek();
+void display();
+
 int main(){
+    pop();
+    push(5);
+    push(1);
+    pop();
+    push(3);
+    push(4);
+    push(5);
+    push(8);
+    push(9);
+    peek();
+    display();
+    
 	return 0;
+}
+
+void display(){
+    printf("\n");
+    for(int i=0; i<=TOP; i++){
+        printf("%d |",stack[i]);
+    }
 }
 
 // 
 void push(int x){
 	if(TOP >= MAXSIZE-1){
-		printf("Stack overflow - Stack is full.\n");
+		printf("\nStack overflow - Stack is full.\n");
 	}else{
 		TOP++;
 		stack[TOP] = x;
+		printf("\n%d is pushed to stack.",x);
 	}
 }
 
 int pop(){
 	if(TOP==-1){
-		printf("Stack is empty.");
+		printf("\nStack is empty.");
+		return -1;
 	}else{
 		int x = stack[TOP];
 		TOP--;
+		printf("\n%d is popped from stack.",x);
 		return x;
 	}
 }
 
+int peek(){
+	if(TOP==-1){
+		printf("\nStack is empty.");
+		return -1;
+	}else{
+		int x = stack[TOP];
+		printf("\n%d is peeked from stack.",x);
+		return x;
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--------------------------
+Enter your choice.
+Press 1 : Push to stack
+Press 2 : Pop from stack
+Press 3 : Peek from stack
+Press 4 : Display stack
+Press 5 : Exit
 
 	
 	
